e3d08679e2f67fb35b1bd2a74eec165e5d288871
Tanner Patterson <tpatterson5492@gmail.com>
2019-10-01 19:29:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0
   new_array = []
   while array[counter] do
-    put 
+    new_array.push(Math.sqrt(array[counter]))
     counter += 1
+    puts new_array
   end
 end
